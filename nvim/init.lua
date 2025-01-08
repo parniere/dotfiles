b4f5a7c59ebeccdf990169ba788d85a4007cb08e
parniere <40841322+parniere@users.noreply.github.com>
2025-01-08 23:05:13 +0100

    fix: nvim cmp shortcuts

--- nvim/init.lua
+++ nvim/init.lua
@@ -266,13 +266,13 @@ cmp.setup({
 		end,
 	},
 	mapping = {
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.abort(),
+		["<Esc>"] = cmp.mapping.abort(),
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
 		["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
 		["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+		["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+		["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
 	},
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
